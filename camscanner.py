5157c87a63517dcd2116e7f267d4f89ffa034f6c
ayda_naderi <ayda.f.naderi@gmail.com>
2022-01-03 16:00:49 +0330

    'edit'

--- camscanner.py
+++ camscanner.py
@@ -1,29 +1,35 @@
 import cv2
 import numpy as np
 
-circles = np.zeros((4,2),np.int64)
-counter = 0
+point = np.zeros((4,2),np.int64)
+count = 0
 
-def point_camscanner(event,x,y,flags,params):
-    global counter
+def Coordinates(event,x,y,flags,params):
+    global count
     if event == cv2.EVENT_LBUTTONDOWN:
-        circles[counter] = x,y
-        counter = counter + 1
+        point[count] = x,y
+        count = count + 1
+        
 path = input("enter the path of your picture : \n")
 img = cv2.imread(path, 1)
+
 while True :
-    if counter == 4:
-        height , width = 500 , 600
-        points = np.float32([circles[0],circles[1],circles[2],circles[3]])
-        new_points = np.float32([[0,0],[width,0],[0,height],[width,height]])
-        matrix = cv2.getPerspectiveTransform(points,new_points)
-        perspective = cv2.warpPerspective(img,matrix,(width,height))
-        cv2.destroyWindow("original")
-        cv2.imshow("camscanner",perspective)
+    
+    if count == 4:
+        height = 500
+        width = 500
+        p1 = np.float32([point[0],point[1],point[2],point[3]])
+        p2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+        matrix = cv2.getPerspectiveTransform(p1,p2)
+        camscanner = cv2.warpPerspective(img,matrix,(width,height))
+        cv2.destroyWindow("input")
+        cv2.imshow("camscanner",camscanner)
         break
+    
     for x in range(0,4):
-       cv2.circle(img,(circles[x][0],circles[x][1]),3,(255,0,0),cv2.FILLED)
-    cv2.imshow("original",img)
-    cv2.setMouseCallback("original",point_camscanner)
+       cv2.circle(img,(point[x][0],point[x][1]),2,(255,0,0),cv2.FILLED)
+       
+    cv2.imshow("input",img)
+    cv2.setMouseCallback("input",Coordinates)
     cv2.waitKey(1)
 
